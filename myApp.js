dab6c81ec8ea447febac0958c51bbcde766806a1
Ілля Наливайко <>
2022-08-09 20:49:49 +0000

    Add getting file by its absolute path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+const absolutePath = __dirname + '/views/index.html';
+app.get('/', (req, res) => {
+  res.sendFile(absolutePath);
+});
+
 
 
 
