cbd74d144f469471570926f991748e0e99567219
Illia <95241786+Nailgy@users.noreply.github.com>
2022-08-09 21:04:12 +0000

    Creating middleware to get CSS file with 'app.use()'

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ const absolutePath = __dirname + '/views/index.html';
 app.get('/', (req, res) => {
   res.sendFile(absolutePath);
 });
+const absolutePath2 = __dirname + '/public';
+app.use('/public', express.static(absolutePath2));
+
 
 
 
